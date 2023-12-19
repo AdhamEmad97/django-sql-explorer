344a192424695740a25ccbf13f690d43c3c1d25b
Chris Clark <chris@untrod.com>
2023-12-19 10:15:57 -0500

    avg duration

--- explorer/models.py
+++ explorer/models.py
@@ -62,6 +62,9 @@ class Query(models.Model):
     def get_run_count(self):
         return self.querylog_set.count()
 
+    def avg_duration_display(self):
+        return "{:10.3f}".format(self.avg_duration())
+
     def avg_duration(self):
         return self.querylog_set.aggregate(
             models.Avg('duration')
