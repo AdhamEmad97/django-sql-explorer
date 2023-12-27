4e0f639fcf595c47056385762b000e4ff616afc4
Chris Clark <chris@untrod.com>
2023-12-27 09:40:43 -0500

    resolving conflicts

--- explorer/templatetags/vite.py
+++ explorer/templatetags/vite.py
@@ -9,7 +9,7 @@ register = template.Library()
 
 VITE_OUTPUT_DIR = "/static/"
 VITE_MANIFEST_FILE = "./explorer/public/static/.vite/manifest.json"
-VITE_DEV_MODE = getattr(settings, "VITE_DEV_MODE", getattr(settings, "EXPLORER_DEBUG", False))
+VITE_DEV_MODE = getattr(settings, "VITE_DEV_MODE", False)
 VITE_SERVER_HOST = getattr(settings, "VITE_SERVER_HOST", "localhost")
 VITE_SERVER_PORT = getattr(settings, "VITE_SERVER_PORT", "5173")
 
